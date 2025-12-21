8bf45d0844d8321749674043bc16e9fdc971cefa
Mustafa Emre Geçim <mustafaemregecim7@gmail.com>
2025-12-21 22:08:26 +0300

    Update

--- Streamlit_App/main.py
+++ Streamlit_App/main.py
@@ -1,12 +1,12 @@
-import Streamlit_App as st
+import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Airbnb", page_icon="air.png")
 
-home_page = st.Page(page="page/homepage.py",title="Tanıtım", icon="🌎")
-analytics_page = st.Page(page="page/analyticspage.py" , title="Veriseti" ,icon= "📊" )
-recommender_page = st.Page(page="page/recommender.py" , title="recommender" ,icon= "🏖️" )
+# Klasör yapın düz olduğu için "page/" kısmını sildik
+home_page = st.Page(page="homepage.py", title="Tanıtım", icon="🌎")
+analytics_page = st.Page(page="analyticspage.py", title="Veriseti", icon="📊")
+recommender_page = st.Page(page="recommender.py", title="Öneri Sistemi", icon="🏖️")
 
-pg = st.navigation([home_page , analytics_page,recommender_page])
+pg = st.navigation([home_page, analytics_page, recommender_page])
 
 pg.run()
-
